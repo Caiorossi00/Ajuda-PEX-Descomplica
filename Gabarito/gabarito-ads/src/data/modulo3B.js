fccd83053b6f6300e919d42e2b3d53a5047f865d
Caiorossi00 <caio_rossi56@hotmail.com>
2025-11-10 11:40:29 -0300

    feat: expand modulo3B data with new exercises and topics on API creation, authentication, database management, error handling, and API versioning

--- Gabarito/gabarito-ads/src/data/modulo3B.js
+++ Gabarito/gabarito-ads/src/data/modulo3B.js
@@ -68,10 +68,176 @@ export const modulo3B = {
             "A programação reativa melhora a escalabilidade e a resiliência de uma aplicação web ao permitir o processamento assíncrono e não bloqueante de eventos, otimizando o uso de recursos e possibilitando que o sistema lide com múltiplas requisições simultâneas de forma eficiente. Em um sistema de notificações em tempo real, por exemplo, cada evento disparado, como uma nova mensagem ou curtida — é tratado como um fluxo de dados contínuo, permitindo que as notificações sejam enviadas instantaneamente a vários usuários sem sobrecarregar o servidor. Além disso, caso ocorra uma falha em algum componente, o sistema reativo pode redirecionar fluxos, retentar operações e manter a aplicação estável, garantindo alta disponibilidade e resposta rápida mesmo sob grande volume de eventos.",
           ],
         },
+        {
+          id: 3,
+          titulo: "Criação de uma API com Java e Spring Boot",
+          exercicios: [
+            { numero: 1, resposta: "C - Like" },
+            {
+              numero: 2,
+              resposta: "C - Persistir dados com Hibernate.",
+            },
+            {
+              numero: 3,
+              resposta: "B - Definir a classe como um controlador REST.",
+            },
+            {
+              numero: 4,
+              resposta:
+                "B - Ajudar a entender a estrutura e os relacionamentos entre as tabelas do banco de dados.",
+            },
+            {
+              numero: 5,
+              resposta: "D - Spring Batch",
+            },
+            {
+              numero: 6,
+              resposta: "B - Apenas as afirmações II, III e V estão corretas.",
+            },
+          ],
+        },
+        {
+          id: 4,
+          titulo: "Autenticação e Autorização",
+          exercicios: [
+            {
+              numero: 1,
+              resposta:
+                "B - Validar a identidade do usuário que está tentando acessar o sistema.",
+            },
+            {
+              numero: 2,
+              resposta:
+                "D - Definir e controlar o que um usuário autenticado pode fazer dentro do sistema.",
+            },
+            {
+              numero: 3,
+              resposta: "A - @EnableWebSecurity",
+            },
+            {
+              numero: 4,
+              resposta: "E - BCryptPasswordEncoder()",
+            },
+            {
+              numero: 5,
+              resposta:
+                "C - Interceptar requisições HTTP e validar tokens JWT.",
+            },
+            {
+              numero: 6,
+              resposta: "E - Apenas I, II, III e IV.",
+            },
+          ],
+        },
+        {
+          id: 5,
+          titulo: "Acrescentando Banco de Dados",
+          exercicios: [
+            {
+              numero: 1,
+              resposta:
+                "B - Gerenciar, organizar e recuperar dados de forma eficiente.",
+            },
+            {
+              numero: 2,
+              resposta: "D - MySQL Workbench.",
+            },
+            {
+              numero: 3,
+              resposta: "C - CREATE DATABASE FrameBlog.",
+            },
+            {
+              numero: 4,
+              resposta:
+                "E - ​spring.datasource.url=jdbc:mysql://localhost:3306/FrameBlog",
+            },
+            {
+              numero: 5,
+              resposta: "B - Flyway​",
+            },
+            {
+              numero: 6,
+              resposta: "D - II, III, IV e V",
+            },
+          ],
+        },
+        {
+          id: 6,
+          titulo: "Tratamento de falhas e exceções",
+          exercicios: [
+            {
+              numero: 1,
+              resposta:
+                "B - Falhas são erros inesperados causados por fatores externos, como problemas de hardware.",
+            },
+            {
+              numero: 2,
+              resposta:
+                "D - Capturar e tratar exceções que ocorrem no bloco `try`.",
+            },
+            {
+              numero: 3,
+              resposta:
+                "A - Para executar um código independentemente de ter ocorrido uma falha ou não.",
+            },
+            {
+              numero: 4,
+              resposta:
+                "C - Uma condição prevista ou imprevista na lógica do programa que pode ser tratada.",
+            },
+            {
+              numero: 5,
+              resposta:
+                "E - Criar mensagens de erro específicas e informativas para facilitar a depuração.​",
+            },
+            {
+              numero: 6,
+              resposta: "B- ​Apenas I, II e V estão corretas.",
+            },
+          ],
+          penseEResponda: ["B - Apenas II, III e V estão corretas."],
+        },
+        {
+          id: 7,
+          titulo: "Versionamento de API",
+          exercicios: [
+            {
+              numero: 1,
+              resposta:
+                "B - Manter diferentes versões da API para suportar funcionalidades novas e antigas.",
+            },
+            {
+              numero: 2,
+              resposta: "D - Utilização de métodos POST para versionamento",
+            },
+            {
+              numero: 3,
+              resposta:
+                "A - Para facilitar a implementação, teste e verificação de contratos e dependências.",
+            },
+            {
+              numero: 4,
+              resposta:
+                "B - Mantém a URL base limpa e evita a necessidade de múltiplas URLs.",
+            },
+            {
+              numero: 5,
+              resposta:
+                "A - Permitir que diferentes clientes utilizem a versão da API que melhor atende às suas necessidades.​",
+            },
+            {
+              numero: 6,
+              resposta: "A - Apenas as afirmações I, II e IV estão corretas.",
+            },
+          ],
+        },
       ],
       listaDeRevisao: [
-        { numero: 1, resposta: "Lorem Ipsum" },
-        { numero: 2, resposta: "Simet Dolor" },
+        { numero: 1, resposta: "A - I, II, III." },
+        { numero: 2, resposta: "B - II, III." },
+        { numero: 3, resposta: "C - I, IV." },
+        { numero: 4, resposta: "A - I, III, V." },
+        { numero: 5, resposta: "A - I, IV." },
       ],
     },
   ],
